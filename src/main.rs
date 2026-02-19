efe354d16c3041853ce78e606877dc9f34966e83
tomoya-matsuura-fb <tomoya.matsuura@flinters-base.co.jp>
2026-02-20 08:10:45 +0900

    feat: 緑画面の表示

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![no_std]
 #![no_main]
-#![feature(offset_of)]
+//#![feature(offset_of)]
 
 // use core::panic::PanicInfo;
 // use core::ptr::read_volatile;
@@ -35,11 +35,14 @@ use core::mem::offset_of;
 use core::mem::size_of;
 use core::panic::PanicInfo;
 use core::ptr::null_mut;
-use core::slice;
+// use core::slice;
+use core::cmp::min;
+use uefi::prelude::Status;
+
 
 type EfiVoid = u8;
 type EfiHandle = u64;
-type Result<T> = core::result::Result<T, &'static str>;
+//type Result<T> = core::result::Result<T, &'static str>;
 
 #[repr(C)]
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
@@ -80,7 +83,7 @@ struct EfiSystemTable {
     _reserved0: [u64; 12],
     pub boot_services: &'static EfiBootServicesTable,
 }
-const _: () = assert!(offset_of!(EfiSystemTable, boot_services) ==96);
+const _: () = assert!(offset_of!(EfiSystemTable, boot_services) == 96);
 
 #[repr(C)]
 #[derive(Debug)]
@@ -113,7 +116,7 @@ struct EfiGraphicsOutputProtocol<'a> {
 }
 fn locate_graphic_protocol<'a>(
     efi_system_table: &EfiSystemTable,
-) -> Result<&'a EfiGraphicsOutputProtocol<'a>> {
+) -> Result<&'a EfiGraphicsOutputProtocol<'a>, uefi::prelude::Status> {
     let mut graphic_output_protocol = null_mut::<EfiGraphicsOutputProtocol>();
     let status = (efi_system_table.boot_services.locate_protocol)(
         &EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID,
@@ -122,7 +125,7 @@ fn locate_graphic_protocol<'a>(
             as *mut *mut EfiVoid,
     );
     if status != EfiStatus::Success {
-        return Err("Failed to locate graphics output protocol");
+        return Err(Status::NOT_FOUND);
     }
     Ok(unsafe { &*graphic_output_protocol })
 }
@@ -133,7 +136,8 @@ pub fn hlt() {
 
 #[no_mangle]
 fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
-    let efi_graphics_output_protocol = locate_graphic_protocol(efi_system_table).unwrap();
+    /*let efi_graphics_output_protocol =
+        locate_graphic_protocol(efi_system_table).unwrap();
     let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
     let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_size;
     let vram = unsafe {
@@ -143,7 +147,21 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
         )
     };
     for e in vram {
-        *e = 0xffffff;
+        *e = 0xffffff;*/
+    let mut vram = init_vram(efi_system_table).expect("init_vram failed");
+    for y in 0..vram.height {
+        for x in 0..vram.width {
+            if let Some(pixel) = vram.pixel_at_mut(x, y) {
+                *pixel = 0x00ff00;
+            }
+        }
+    }
+    for y in 0..vram.height / 2 {
+        for x in 0..vram.width / 2 {
+            if let Some(pixel) = vram.pixel_at_mut(x, y) {
+                *pixel = 0xff0000;
+            }
+        }
     }
     //println!("Hello, world!");
     //loop {}
@@ -158,7 +176,7 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
 //     let loaded_image_protocol =
 //         locate_loaded_image_protocol(image_handle, efi_system_table)
 //             .expect("Failed to get LoadedImageProtocol");
-// 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 
+// 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
 //     println!("image_base: {:#018X}", loaded_image_protocol.image_base);
 //     println!("image_size: {:#018X}", loaded_image_protocol.image_size);
 //     info!("info");
@@ -177,13 +195,13 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
 //     init_paging(&memory_map);
 //     init_hpet(acpi);
 //     init_pci(acpi);
-//     let serial_task = 
+//     let serial_task =
 //         let sp = SerialPort::default();
 //         if let Err(e) = sp.loopback_test() {
 //             error!("{e:?}");
 //             return Err("serial: loopback test failed");
 //         }
-//         iasync 
+//         iasync
 //             nfo!("Started to monitor serial port");
 //         loop {
 //             if let Some(v) = sp.try_read() {
@@ -222,14 +240,78 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
 //     start_global_executor()
 // }
 
-
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-//     error!("PANIC: {info:?}");
-//     exit_qemu(QemuExitCode::Fail);
-// }
+    //     error!("PANIC: {info:?}");
+    //     exit_qemu(QemuExitCode::Fail);
+    // }
     //loop {}
     loop {
         hlt()
     }
 }
+
+trait Bitmap {
+    fn bytes_per_pixel(&self) -> i64;
+    fn pixels_per_line(&self) -> i64;
+    fn width(&self) -> i64;
+    fn height(&self) -> i64;
+    fn buf_mut(&mut self) -> *mut u8;
+    unsafe fn unchecked_pixel_at_mut(&mut self, x: i64, y: i64) -> *mut u32 {
+        self.buf_mut().add(
+            ((y * self.pixels_per_line() + x) * self.bytes_per_pixel())
+                as usize,
+        ) as *mut u32
+    }
+    fn pixel_at_mut(&mut self, x: i64, y: i64) -> Option<&mut u32> {
+        if self.is_in_x_range(x) && self.is_in_y_range(y) {
+            unsafe { Some(&mut *(self.unchecked_pixel_at_mut(x, y))) }
+        } else {
+            None
+        }
+    }
+    fn is_in_x_range(&self, px: i64) -> bool {
+        0 <= px && px < min(self.width(), self.pixels_per_line())
+    }
+    fn is_in_y_range(&self, py: i64) -> bool {
+        0 <= py && py < self.height()
+    }
+}
+
+#[derive(Clone, Copy)]
+struct VramBufferInfo {
+    buf: *mut u8,
+    width: i64,
+    height: i64,
+    pixels_per_line: i64,
+}
+
+impl Bitmap for VramBufferInfo {
+    fn bytes_per_pixel(&self) -> i64 {
+        4
+    }
+    fn pixels_per_line(&self) -> i64 {
+        self.pixels_per_line
+    }
+    fn width(&self) -> i64 {
+        self.width
+    }
+    fn height(&self) -> i64 {
+        self.height
+    }
+    fn buf_mut(&mut self) -> *mut u8 {
+        self.buf
+    }
+}
+
+fn init_vram(efi_system_table: &EfiSystemTable) -> Result<VramBufferInfo, uefi::Status> {
+    let gp = locate_graphic_protocol(efi_system_table)?;
+    Ok(VramBufferInfo {
+        buf: gp.mode.frame_buffer_base as *mut u8,
+        width: gp.mode.info.horizontal_resolution as i64,
+        height: gp.mode.info.vertical_resolution as i64,
+        pixels_per_line: gp.mode.info.pixels_per_scan_line as i64,
+    })
+}
+
+
